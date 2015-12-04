e433b879153565d77e4b28dc3f48b9767472b2dc
Simon Laing <laingster@gmail.com>
2015-12-04 22:36:01 +0000

    Updated to facilitate issue #1 (now supports printing of the previewed image)

--- PreviewIo/PreviewControl.Designer.cs
+++ PreviewIo/PreviewControl.Designer.cs
@@ -28,8 +28,12 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.picPreview = new System.Windows.Forms.PictureBox();
+			this.mnuContext = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.itmPrint = new System.Windows.Forms.ToolStripMenuItem();
 			((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+			this.mnuContext.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// picPreview
@@ -42,13 +46,29 @@
 			this.picPreview.TabIndex = 0;
 			this.picPreview.TabStop = false;
 			// 
+			// mnuContext
+			// 
+			this.mnuContext.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itmPrint});
+			this.mnuContext.Name = "mnuContext";
+			this.mnuContext.Size = new System.Drawing.Size(100, 26);
+			// 
+			// itmPrint
+			// 
+			this.itmPrint.Name = "itmPrint";
+			this.itmPrint.Size = new System.Drawing.Size(99, 22);
+			this.itmPrint.Text = "Print";
+			this.itmPrint.Click += new System.EventHandler(this.itmPrint_Click);
+			// 
 			// PreviewControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ContextMenuStrip = this.mnuContext;
 			this.Controls.Add(this.picPreview);
 			this.Name = "PreviewControl";
 			((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+			this.mnuContext.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -56,5 +76,7 @@
 		#endregion
 
 		private System.Windows.Forms.PictureBox picPreview;
+		private System.Windows.Forms.ContextMenuStrip mnuContext;
+		private System.Windows.Forms.ToolStripMenuItem itmPrint;
 	}
 }
--- PreviewIo/PreviewControl.cs
+++ PreviewIo/PreviewControl.cs
@@ -1,4 +1,7 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
 
 namespace PreviewIo
@@ -12,6 +15,41 @@ namespace PreviewIo
 			_context = context;
 			InitializeComponent();
 			picPreview.Image = preview;
+		}
+
+		private void itmPrint_Click(object sender, System.EventArgs e)
+		{
+			try
+			{
+				var tempFile = Path.ChangeExtension(Path.GetTempFileName(), ".png");
+				picPreview.Image.Save(tempFile, ImageFormat.Png);
+
+				var process = Process.Start(new ProcessStartInfo
+				{
+					FileName = tempFile,
+					UseShellExecute = true,
+					Verb = "print"
+				});
+				process.EnableRaisingEvents = true;
+				process.Exited += (s, args) => _DeleteTempFile(tempFile);
+			}
+			catch (System.Exception exc)
+			{
+				ParentForm.Controls.Clear();
+				ParentForm.Controls.Add(new ErrorControl(exc)
+				{
+					Dock = DockStyle.Fill
+				});
+			}
+		}
+
+		private static void _DeleteTempFile(string filePath)
+		{
+			try
+			{
+				File.Delete(filePath);
+			}
+			catch { }
 		}
 	}
 }
