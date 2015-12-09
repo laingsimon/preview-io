1c3e77199f741abe7da0b55ee95190f8aac1b685
Simon Laing <laingster@gmail.com>
2015-12-09 21:23:41 +0000

    Issue #8: Created simplistic install/uninstall scripts

--- install.bat
+++ install.bat
@@ -1,5 +1,13 @@
 @set netframework32=c:\Windows\Microsoft.NET\Framework64\v4.0.30319\
 @set netframework64=c:\Windows\Microsoft.NET\Framework\v4.0.30319\
 @set path=%netframework32%;%netframework64%;%path%
+@set installDir=%ProgramFiles%\PreviewIo
 
-regasm.exe PreviewIo\bin\Debug\PreviewIo.dll /codebase
+@mkdir "%installDir%" 2> nul
+
+@xcopy "PreviewIo\bin\Release\*.*" "%installDir%\" /Y
+@copy uninstall.bat "%installDir%\" /Y
+
+@regasm.exe "%installDir%\PreviewIo.dll" /codebase /nologo /silent
+
+@if "%errorlevel%"=="0" @echo Installed successfully
