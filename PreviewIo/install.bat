0f605d72114848a2bb2154542869c018be9458a3
Simon Laing <simon.laing@kctrust.co.uk>
2017-02-07 19:55:42 +0000

    Updated install script to correctly handle the switch of working directory when installing from a share (UNC path)

--- PreviewIo/install.bat
+++ PreviewIo/install.bat
@@ -2,12 +2,23 @@
 @set netframework64=c:\Windows\Microsoft.NET\Framework\v4.0.30319\
 @set path=%netframework32%;%netframework64%;%path%
 @set installDir=%ProgramFiles%\PreviewIo
+@set source=%~dp0
 
 @mkdir "%installDir%" 2> nul
 
-@xcopy "*.*" "%installDir%\" /Y
-@copy uninstall.bat "%installDir%\" /Y
+@copy "%source%\uninstall.bat" "%installDir%\" /Y
+@if not exist "%installDir%\uninstall.bat" goto error
+
+@xcopy "%source%*.*" "%installDir%\" /Y
 
 @regasm.exe "%installDir%\PreviewIo.dll" /codebase /nologo /silent
 
 @if "%errorlevel%"=="0" @echo Installed successfully
+@goto eof
+
+:error
+@echo Error copying files
+@goto eof
+
+:eof
+@pause
